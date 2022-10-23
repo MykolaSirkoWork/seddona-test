84971669488e1b34553a137df657cf71feb31fac
Mykola Sirko <mykola.sirko.work@gmail.com>
2022-10-23 15:41:42 +0300

    create header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import Header from "./components/layout/Header";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
---
+++ src/components/layout/Header.js
@@ -0,0 +1,50 @@
+
+import searctIcon from '../../assets/images/search-icon.svg';
+import wallerIcon from '../../assets/images/wallet-icon.svg';
+import profileIcon from '../../assets/images/profile-icon.svg';
+import mainLogo from '../../assets/images/main-logo.png';
+
+const links = [
+  { path: '/games', label: 'GAMES' },
+  { path: '/items', label: 'ITEMS' },
+  { path: '/download', label: 'DOWNLOAD' },
+]
+
+const Header = () => {
+  return (
+    <div className="header container">
+      <div className="header__company-logo">
+        <a href="/"><img src={mainLogo} /></a>
+      </div>
+      <div className="d-flex">
+        <nav className="d-flex f-ai-c">
+          <ul className="nav-links">
+            {links.map(link => (
+              <li key={link.label}>
+                <a
+                  className={`${link.label === 'DOWNLOAD' ? 'download-link' : ''}`}
+                  href={link.path}
+                >
+                  {link.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className="header__manage-btns">
+          <button className="search-btn">
+            <img src={searctIcon} />
+          </button>
+          <div className="icon-link">
+            <a href="/"><img src={profileIcon} /></a>
+          </div>
+          <div className="icon-link">
+            <a href="/"><img src={wallerIcon} /></a>
+          </div>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
