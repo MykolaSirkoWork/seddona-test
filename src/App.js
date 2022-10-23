4220a9656cf8dcb60a63e6086f700d68e08e4537
Mykola Sirko <mykola.sirko.work@gmail.com>
2022-10-23 15:52:38 +0300

    create footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,28 @@
+import twitterIcon from '../../assets/images/twitter-icon.svg';
+import discordIcon from '../../assets/images/discord-icon.svg';
+import footerLogo from '../../assets/images/footer-logo.png';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="container d-flex f-ji-sb f-ai-c">
+        <div className="footer__logo-container">
+          <img src={footerLogo} alt="ompany logo" />
+          <p className="footer__rights">&copy; Seddona 2022</p>
+        </div>
+        <div className="footer__links">
+          <ul className="footer__nav-links">
+            <li><a href="/">About</a></li>
+            <li><a href="/">Support</a></li>
+          </ul>
+          <ul className="footer__social-links">
+            <li><a href="/"><img src={twitterIcon} alt="twitter icon"/></a></li>
+            <li><a href="/"><img src={discordIcon} alt="discord icon"/></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
