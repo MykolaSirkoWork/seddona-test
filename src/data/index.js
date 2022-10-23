dac6d806d5ae97f6d37cd03c6fe9cd45ec784874
Mykola Sirko <mykola.sirko.work@gmail.com>
2022-10-23 15:24:53 +0300

    add basic styles

---
+++ src/data/index.js
@@ -0,0 +1,22 @@
+export const games = [
+  {
+    title: 'Yeti Bomber',
+    img: gameImg,
+    isReleased: true
+  },
+  {
+    title: 'Yeti Bomber',
+    img: gameImg,
+    isReleased: false
+  },
+  {
+    title: 'Yeti Bomber',
+    img: gameImg,
+    isReleased: false
+  },
+  {
+    title: 'Yeti Bomber',
+    img: gameImg,
+    isReleased: true
+  }
+]
